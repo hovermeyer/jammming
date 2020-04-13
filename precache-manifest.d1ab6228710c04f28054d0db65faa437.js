fda7e27d480a1a898e66e2f25cdc61ed40a70e80
Heather Overmeyer <heather.overmeyer@gmail.com>
2020-04-13 16:36:05 -0400

    Updates

---
+++ precache-manifest.d1ab6228710c04f28054d0db65faa437.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb038180735d9ffbe217b4757cffadf8",
+    "url": "/jammming/index.html"
+  },
+  {
+    "revision": "fd03e6cc6d03e0b4035c",
+    "url": "/jammming/static/css/main.544aebcd.chunk.css"
+  },
+  {
+    "revision": "285a8b9078a69e49b876",
+    "url": "/jammming/static/js/2.7abe92db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jammming/static/js/2.7abe92db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd03e6cc6d03e0b4035c",
+    "url": "/jammming/static/js/main.68eae23f.chunk.js"
+  },
+  {
+    "revision": "9399b5a2b7d0cc176deb",
+    "url": "/jammming/static/js/runtime-main.dc6fbbb7.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jammming/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
